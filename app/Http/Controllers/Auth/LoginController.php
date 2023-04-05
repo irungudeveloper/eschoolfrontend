a85f230160254643f4f52ef218f7e03db74129ac
irungudeveloper <irunguedwin8042@gmail.com>
2023-04-06 01:47:35 +0300

    frontend in earnest

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Http;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    // use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    // protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    // public function __construct()
+    // {
+    //     $this->middleware('guest')->except('logout');
+    // }
+
+
+    public function login()
+    {
+        # code...
+        $response = Http::post('http://localhost:8001/api/login', [
+            'email' => 'irungu_applicant_new@gmail.com',
+            'password' => 'qwerty1234',
+        ]);
+
+        return response()->json($response->body());
+
+    }
+
+}
